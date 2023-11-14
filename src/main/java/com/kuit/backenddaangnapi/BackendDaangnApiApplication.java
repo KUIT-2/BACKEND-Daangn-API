1089de56b13715552d7f7cebc6e9c07ecfc6757a
Han Ju Kim <hjkim2902@gmail.com>
2023-11-14 16:15:03 +0900

    init commit

---
+++ src/main/java/com/kuit/backenddaangnapi/BackendDaangnApiApplication.java
@@ -0,0 +1,13 @@
+package com.kuit.backenddaangnapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDaangnApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDaangnApiApplication.class, args);
+    }
+
+}
